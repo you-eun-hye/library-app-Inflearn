f73bdda16ea081968701409dcf465fcf3ab7d8cf
you-eun-hye <dkfvktorcp@naver.com>
2023-02-22 21:11:15 +0900

    2강까지의 코드 : MySQL 연동

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,14 +1,25 @@
 package com.group.libraryapp.controller.calculator;
 
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
 
     @GetMapping("/add") // GET /add
-    public int addTwoNumbers(@RequestParam int number1, @RequestParam int number2){
-        return number1 + number2;
+    public int addTwoNumbers(CalculatorAddRequest request){
+
+        return request.getNumber1() + request.getNumber2();
+
+    }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request){
+
+        return request.getNumber1() * request.getNumber2();
     }
 }
