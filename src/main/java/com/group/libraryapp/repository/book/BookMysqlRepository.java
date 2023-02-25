324b71c98f005fa2c261605e407a8481b4ccba77
you-eun-hye <dkfvktorcp@naver.com>
2023-02-25 15:45:39 +0900

    4강까지 : 3강 스프링 컨테이너, JPA, 트랜잭션

---
+++ src/main/java/com/group/libraryapp/repository/book/BookMysqlRepository.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.repository.book;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+@Primary
+@Repository
+public class BookMysqlRepository implements  BookRepository{
+
+    @Override
+    public void saveBook() {
+
+    }
+}
+
