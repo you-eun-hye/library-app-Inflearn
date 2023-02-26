0b060470c37c30d691236e902d7cdcf4280814a3
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-02-26 20:51:12 +0900

    error 발생

--- src/main/java/com/group/libraryapp/domain/book/Book.java
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -1,47 +1,29 @@
-package com.group.libraryapp.domain.user;
+package com.group.libraryapp.domain.book;
 
 import javax.persistence.*;
 
 @Entity
-public class User {
+public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id = null;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false)
     private String name;
-    private Integer age;
 
-    protected User(){}
+    protected Book(){
 
-    public User(String name, Integer age) {
+    }
+
+    public Book(String name) {
         if(name == null || name.isBlank()){
             throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다.", name));
         }
         this.name = name;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
         return name;
     }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void updateName(String name){
-        this.name = name;
-    }
-    
 }
-
-
-
-
-
