6a134b8cc1f840fc984516b7911a9ef77a6cc77c
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-03-04 14:22:54 +0900

    before upgrading object oriented

--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -25,4 +25,8 @@ public class UserLoanHistory {
         this.bookName = bookName;
         this.isReturn = false;
     }
+
+    public void doReturn(){
+        this.isReturn = true;
+    }
 }
--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserLoanHistoryRepository extends JpaRepository<UserLoanHistory, Long> {
     boolean existsByBookNameAndIsReturn(String name, boolean isReturn);
 
+    Optional<UserLoanHistory> findByUserIdAndBookName(long userId, String bookName);
+
 }
