2c9480df088eaeb928f21d056118afd9c298f44b
you-eun-hye <dkfvktorcp@naver.com>
2023-03-04 16:26:12 +0900

    after refactoring

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,6 +1,10 @@
 package com.group.libraryapp.domain.user;
 
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User {
@@ -12,6 +16,9 @@ public class User {
     @Column(nullable = false, length = 20)
     private String name;
     private Integer age;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserLoanHistory> userLoanHistories = new ArrayList<>();
 
     protected User(){}
 
@@ -38,7 +45,25 @@ public class User {
     public void updateName(String name){
         this.name = name;
     }
-    
+
+    public void loanBook(String bookName){
+        this.userLoanHistories.add(new UserLoanHistory(this, bookName));
+    }
+
+    public void returnBook(String bookName){
+        // 1. 함수형 프로그래밍을 할 수 있게, stream을 시작한다.
+        UserLoanHistory targetHistory = this.userLoanHistories.stream()
+                // 2. 들어오는 객체들 중 다음 조건을 충족하는 것만 필터링 한다.
+                // 조건 : UserLoanHistory 중 책 이름이 bookName과 같은 것
+                .filter(history -> history.getBookName().equals(bookName))
+                // 3. 첫 번째로 해당하는 UserLoanHistory를 찾는다.
+                .findFirst()
+                // Optional을 제거하기 위해 없으면 예외를 던진다.
+                .orElseThrow(IllegalArgumentException::new);
+        // 4. 그렇게 찾은 UserLoanHistory를 반납처리 한다.
+        targetHistory.doReturn();
+    }
+
 }
 
 
